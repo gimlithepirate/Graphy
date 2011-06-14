f1724e9b1dea488ce92a81b24a64cb6362fc10b3
Peter Mack Grubb <gimlithepirate@Peter-Mack-Grubbs-Mac-mini.local>
2011-06-14 10:30:41 -0500

    Added a setup.py file to the repo.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils import setup
+
+setup(
+    name = 'Graphy',
+    version = '1.0.1',
+    packages = ('Graphy',),
+    license = 'Apache 2.0',
+    long_description = open('README.txt').read(),
+)
