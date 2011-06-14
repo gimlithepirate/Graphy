6c8ce3e748bca90b731a03877b799d8e8711011a
Peter Mack Grubb <gimlithepirate@Peter-Mack-Grubbs-Mac-mini.local>
2011-06-14 10:33:40 -0500

    Update to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils import setup
+from distutils.core import setup
 
 setup(
     name = 'Graphy',
     version = '1.0.1',
+    description = "A fork of graphy that uses HTTPS for the graph files"
     packages = ('Graphy',),
     license = 'Apache 2.0',
     long_description = open('README.txt').read(),
