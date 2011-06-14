1e7447e75b5cec4436232bb96cdfc81e795470bf
Peter Mack Grubb <gimlithepirate@Peter-Mack-Grubbs-Mac-mini.local>
2011-06-14 10:40:56 -0500

    Deleted the line in setup referring to the README file. File is still avail able on github.

--- setup.py
+++ setup.py
@@ -6,5 +6,4 @@ setup(
     description = "A fork of graphy that uses HTTPS for the graph files",
     license = 'Apache 2.0',
     packages = ['Graphy',],
-    long_description = open('README.txt').read(),
 )
