83feb56a35c8877a6dc7f526b303ed2ad2c9615b
Peter Mack Grubb <gimlithepirate@Peter-Mack-Grubbs-Mac-mini.local>
2011-06-14 10:39:49 -0500

    More attempts to fix the lingering syntax error.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'Graphy',
     version = '1.0.1',
     description = "A fork of graphy that uses HTTPS for the graph files",
-    packages = ['Graphy',],
     license = 'Apache 2.0',
+    packages = ['Graphy',],
     long_description = open('README.txt').read(),
 )
